6be98c6c2d97ab1f73bcd8d9c650b237f24f7c8f
Prototype-Km <kgm970910@gmail.com>
2025-05-15 08:03:01 +0900

    refactor : Lv1.트랜잭션,Lv1-3.todos ,Lv8. getTodos

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -2,16 +2,30 @@ package org.example.expert.domain.todo.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
+import org.example.expert.domain.todo.dto.request.TodoSearchDTO;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponseDTO;
+import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
@@ -28,14 +42,31 @@ public class TodoController {
 
     @GetMapping("/todos")
     public ResponseEntity<Page<TodoResponse>> getTodos(
-            @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
-    ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
+            @RequestParam(value = "page", defaultValue = "1") int clientPage,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(required = false) String startDate,
+            @RequestParam(required = false) String endDate,
+            @RequestParam(required = false) String weather){
+        Pageable pageable = PageRequest.of(clientPage - 1 ,size, Sort.by("modifiedAt").descending());
+        return ResponseEntity.ok(todoService.getTodos(startDate,endDate,weather,pageable));
     }
 
     @GetMapping("/todos/{todoId}")
     public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
         return ResponseEntity.ok(todoService.getTodo(todoId));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<TodoSearchResponseDTO>> search(
+            @RequestParam(value = "page", defaultValue = "1") int clientPage,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false) @DateTimeFormat(iso = DATE) LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DATE) LocalDate endDate,
+            @RequestParam(required = false) String managerNickName
+    ){
+        Pageable pageable = PageRequest.of(clientPage - 1,size, Sort.by("createdAt").descending());
+        TodoSearchDTO todoSearchDTO = new TodoSearchDTO(keyword,startDate,endDate,managerNickName);
+        return ResponseEntity.ok(todoService.search(todoSearchDTO, pageable));
     }
 }
